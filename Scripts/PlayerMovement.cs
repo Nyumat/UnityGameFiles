20d212c95906e340ac4df84eaf415efcb15cca59
thomas <46255836+Nyumat@users.noreply.github.com>
2020-07-13 23:38:39 -0400

    Add files via upload

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{   // reference to the Rigidbody component rb
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    public float jumpForce = 500f;
+
+    // FixedUpdate over Regular Update because
+    // we are using it to mess with physics.
+    void FixedUpdate()
+    {
+        //Add a foward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+          rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+          rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+
+        if (Input.GetKeyDown("space")
+        {
+          Vector3 atas = new Vector3 (0,100,0);
+          rb.AddForce(atas * speed);
+
+        }
+
+
+
+        }
+    }
+}
